ac7321faab6e37808abf07b2b6afbe24aad305d0
KingaPrivate <kin.stanek@gmail.com>
2021-06-25 22:32:17 +0200

    Add icons

--- src/icons/icons.ts
+++ src/icons/icons.ts
@@ -1,5 +1,16 @@
 import { ReactComponent as search } from './search.svg';
+import { ReactComponent as arrowSortUp } from './arrow_sort_up.svg';
+import { ReactComponent as arrowSortDown } from './arrow_sort_down.svg';
+import { ReactComponent as chevronRight } from './chevron_right.svg';
+import { ReactComponent as chevronLeft } from './chevron_left.svg';
+import { ReactComponent as  moreHoriz} from './more_horiz.svg';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   search,
+  arrowSortUp,
+  arrowSortDown,
+  chevronRight,
+  chevronLeft,
+  moreHoriz
 };
