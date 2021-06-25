79a6c56cdc3bf3c5a1fdd5cea5c0301fc3d2a9e9
KingaPrivate <kin.stanek@gmail.com>
2021-06-25 19:27:39 +0200

    Add dateManager

---
+++ src/utils/dateManager.ts
@@ -0,0 +1,11 @@
+export const formatDate = (date: Date): string => {
+  let month = String(date.getMonth() + 1);
+  let day = String(date.getDate());
+  const year = String(date.getFullYear());
+
+  if (month.length < 2) month = `0${month}`;
+  if (day.length < 2)  day = `0${day}`;
+
+  return [day, month, year].join('/');
+};
+export default formatDate;
