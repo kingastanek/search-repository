0dc3b3060c7fdc4d4a2116fb75caa301404aae12
KingaPrivate <kin.stanek@gmail.com>
2021-06-26 10:43:50 +0200

    Add and update snapshots

--- src/components/Spinner/Spinner.test.tsx
+++ src/components/Spinner/Spinner.test.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
 import { shallow } from 'enzyme';
 import Spinner from './Spinner';
-import { Wrapper } from './Spinner.style';
 
 describe('Spinner', () => {
-  const baseProps = {
-    fixed: true,
-  };
-  const component = shallow(<Spinner {...baseProps} />);
+  const component = shallow(<Spinner />);
 
   it('should render correctly', () => {
     expect(component).toMatchSnapshot();
-  });
-
-  it('should get style for fixed', () => {
-    const styled = shallow(<Wrapper fixed />);
-    expect(styled).toMatchSnapshot();
-  });
-  it('should get style for not fixed', () => {
-    const styled = shallow(<Wrapper fixed={false} />);
-    expect(styled).toMatchSnapshot();
   });
 });
