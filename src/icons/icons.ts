12590830925de1b13bd9dd22d112cf5841f928d2
KingaPrivate <kin.stanek@gmail.com>
2021-06-26 10:40:53 +0200

    Add icons

--- src/icons/icons.ts
+++ src/icons/icons.ts
@@ -4,6 +4,10 @@ import { ReactComponent as arrowSortDown } from './arrow_sort_down.svg';
 import { ReactComponent as chevronRight } from './chevron_right.svg';
 import { ReactComponent as chevronLeft } from './chevron_left.svg';
 import { ReactComponent as  moreHoriz} from './more_horiz.svg';
+import { ReactComponent as  checkCircleOutline } from './check_circle_outline.svg';
+import { ReactComponent as  error } from './error.svg';
+import { ReactComponent as  info } from './info.svg';
+import { ReactComponent as  close } from './close.svg';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -12,5 +16,9 @@ export default {
   arrowSortDown,
   chevronRight,
   chevronLeft,
-  moreHoriz
+  moreHoriz,
+  checkCircleOutline,
+  error,
+  info,
+  close
 };
